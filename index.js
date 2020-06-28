89aa61cfaf45c1a7a7f99a627c96cf7e86b049cd
Indira Pimpalkhare <indira.pimpalkhare@gmail.com>
2020-06-28 10:42:34 +0530

    Added table (status page)

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/status', (req, res) => res.render('pages/status'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
