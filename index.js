b3287c3458daa2b44b07f6f12cc95b4923de60e6
rittmang <ritomgupta99@gmail.com>
2020-06-28 18:21:26 +0530

    Format code

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const path = require('path');
-const fileUpload = require('express-fileupload');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(fileUpload());
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => res.render('pages/index'));
+app.get("/", (req, res) => res.render("pages/index"));
 
-app.get('/status', (req, res) => res.render('pages/status'));
+app.get("/status", (req, res) => res.render("pages/status"));
 
-app.post('/upload', function(req, res) {
+app.post("/upload", function (req, res) {
     if (!req.files || Object.keys(req.files).length === 0) {
         //return res.status(400).send('No files were uploaded.');
         res.redirect("/");
@@ -25,23 +25,21 @@ app.post('/upload', function(req, res) {
     let upload_file = req.files.upload_file;
     var name = upload_file.name;
     // Use the mv() method to place the file somewhere on your server
-    upload_file.mv('uploads/'+ name, function(err) {
-        if (err)
-            return res.status(500).send(err);
-        const directoryPath=path.join(__dirname,'uploads');
-        fs.readdir(directoryPath,function(err,files){
+    upload_file.mv("uploads/" + name, function (err) {
+        if (err) return res.status(500).send(err);
+        const directoryPath = path.join(__dirname, "uploads");
+        fs.readdir(directoryPath, function (err, files) {
             //handling error
-            if(err){
-                return console.log('Unable to scan directory:'+err);
+            if (err) {
+                return console.log("Unable to scan directory:" + err);
             }
             //list all files
-            files.forEach(function(file){
+            files.forEach(function (file) {
                 console.log(file);
-            })
-        })
+            });
+        });
         res.redirect("/");
     });
-    });
+});
 
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
