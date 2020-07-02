8803fbbd2c865dd8e0b4096895219de48c9ef366
Indira Pimpalkhare <indira.pimpalkhare@gmail.com>
2020-07-03 02:10:54 +0530

    Penn

--- routes/pennstate.js
+++ routes/pennstate.js
@@ -17,7 +17,6 @@ router.use(function (req,res,next){
 
 router.get('/:lic_id',  function(req, res){
     var lic_id = req.params.lic_id;
-    var param_name = req.query.param_name;
     var is_api = req.query.is_api;
     var myJSONObject = {"OptPersonFacility":"Person","ProfessionID":13,"LicenseTypeId":156,"LicenseNumber":lic_id,"State":"","Country":"ALL","County": "","IsFacility":0,"PersonId":"","PageNo":1};
     var options =
@@ -30,30 +29,23 @@ router.get('/:lic_id',  function(req, res){
     request(options, function(error, response, body){
         if (!error && response.statusCode == 200) {
             var dict = body[0];
-            //console.log(dict)
+            console.log(dict)
             console.log(dict["FirstName"], dict["MiddleName"], dict["LastName"])
             console.log(dict["Status"])
             console.log(dict["DisciplinaryAction"])
-            var name = dict["FirstName"];
-            if(is_api == "true"){
-                if(param_name == name){
-                    //var tname = 'Indira';
-                    res.send('Verified');
-                    //, {tname : tname});
-                }
-                else{
-                    res.send("Error");
-                }
+            var name = dict["FirstName"]+" " +dict["MiddleName"]+" "+dict["LastName"];
+            if(dict["DisciplinaryAction"] == null){
+                var disc = "No";
             }
             else{
-                if(param_name == name){
-                    //var tname = 'Indira';
-                    res.render('pages/status');
-                    //, {tname : tname});
-                }
-                else{
-                    res.send("uh oh");
-                }
+                var disc = "Yes";
+            }
+            var result = [name,dict["Status"],"EXPIRY DATE",disc];
+            if(is_api == "true"){
+                res.send(JSON.stringify(result));
+            }
+            else{
+                res.render("pages/status",{result:JSON.stringify(result)});
             }
 
         }
