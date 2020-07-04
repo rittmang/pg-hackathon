4d67bf89f19ca8aeb61e1b95ee09723e3ed5aeb2
dheeraj-komandur <dheerajkomandur@gmail.com>
2020-07-04 19:29:01 +0530

    Pen state modified

--- routes/pennstate.js
+++ routes/pennstate.js
@@ -18,38 +18,68 @@ router.use(function (req,res,next){
 router.get('/:lic_id',  function(req, res){
     var lic_id = req.params.lic_id;
     var is_api = req.query.is_api;
-    var myJSONObject = {"OptPersonFacility":"Person","ProfessionID":13,"LicenseTypeId":156,"LicenseNumber":lic_id,"State":"","Country":"ALL","County": "","IsFacility":0,"PersonId":"","PageNo":1};
-    var options =
-        {
-            url: "https://www.pals.pa.gov/api/Search/SearchForPersonOrFacilty",
-            method: "POST",
-            json: true,   // <--Very important!!!
-            body: myJSONObject
-        };
-    request(options, function(error, response, body){
-        if (!error && response.statusCode == 200) {
-            var dict = body[0];
-            console.log(dict)
-            console.log(dict["FirstName"], dict["MiddleName"], dict["LastName"])
-            console.log(dict["Status"])
-            console.log(dict["DisciplinaryAction"])
-            var name = dict["FirstName"]+" " +dict["MiddleName"]+" "+dict["LastName"];
-            if(dict["DisciplinaryAction"] == null){
-                var disc = "No";
-            }
-            else{
-                var disc = "Yes";
-            }
-            var result = [name,dict["Status"],"EXPIRY DATE",disc];
-            if(is_api == "true"){
-                res.send(JSON.stringify(result));
-            }
-            else{
-                res.render("pages/status",{result:JSON.stringify(result)});
-            }
 
+    
+
+    var request = require('request');
+    var options = {
+    'method': 'POST',
+    'url': 'https://www.pals.pa.gov/api/Search/SearchForPersonOrFacilty',
+    'headers': {
+        'Content-Type': 'application/json',
+        'Cookie': 'ASP.NET_SessionId=hhsfm1yzsadz0nmcjn45avhk'
+    },
+    body: JSON.stringify({"OptPersonFacility":"Person","ProfessionID":13,"LicenseTypeId":156,"LicenseNumber":lic_id,"State":"","Country":"ALL","County":null,"IsFacility":0,"PersonId":null,"PageNo":1})
+
+    };
+    request(options, function (error, response) {
+    if (error) throw new Error(error);
+    var temp  = JSON.parse(response.body);
+    var person_Id = temp[0]["PersonId"]
+    var license_Id = temp[0]["LicenseId"]
+
+    var request = require('request');
+    var options = {
+    'method': 'POST',
+    'url': 'https://www.pals.pa.gov/api/SearchLoggedIn/GetPersonOrFacilityDetails',
+    'headers': {
+        'Content-Type': 'application/json',
+        'Cookie': 'ASP.NET_SessionId=hhsfm1yzsadz0nmcjn45avhk'
+    },
+    body: JSON.stringify({"PersonId":person_Id,"LicenseNumber":lic_id,"IsFacility":"0","LicenseId":license_Id})
+
+    };
+    request(options, function (error, response) {
+    if (error) throw new Error(error);
+
+        var temp1  = JSON.parse(response.body);
+        var name = temp1["FirstName"]+temp1["LastName"]
+        var status = temp1["Status"]
+        var expiry = temp1["ExpiryDate"]
+        if (temp1["DisciplinaryActionDetails"].length != 0)
+        {
+            var disi = "Yes"
         }
+        else
+        {
+            var disi = "No"
+        }
+
+        result = [name,status,expiry,disi]
+
+        if(is_api == "true"){
+            res.send(JSON.stringify(result));
+        }
+        else{
+            res.render("pages/status",{result:JSON.stringify(result)});
+        }
+
+
     });
+
+
+    });
+
 
 });
 
