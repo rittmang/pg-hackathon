ed3f37f2805d8124c9cbadaa80369ecd3c5624f8
dheeraj-komandur <dheerajkomandur@gmail.com>
2020-07-04 15:30:17 +0530

    nevada just got disciplined

--- post_to_states/nevada.js
+++ post_to_states/nevada.js
@@ -6,17 +6,40 @@ var options = {
   'headers': {
     'Content-Type': 'application/json'
   },
-  body: JSON.stringify({"sortType":"LicenseNumber","sortOrder":"asc","currentPage":1,"totalRecords":82,"pageSize":10,"maxSize":5,"Data":{"LastName":"","FirstName":"john","LicenseNumber":"0492"}})
+  body: JSON.stringify({"sortType":"LicenseNumber","sortOrder":"asc","currentPage":1,"totalRecords":82,"pageSize":10,"maxSize":5,"Data":{"LastName":"","FirstName":"","LicenseNumber":"0619"}})
 
 };
 request(options, function (error, response) {
   if (error) throw new Error(error);
-    console.log(response.body);
+    //console.log(response.body);
     var temp  = JSON.parse(response.body);
-    user_id = temp["PagerVM"]["Records"][0]["IndividualId"];
-    console.log(temp["PagerVM"]["Records"][0]["FirstName"],temp["PagerVM"]["Records"][0]["MiddleName"],temp["PagerVM"]["Records"][0]["LastName"])
-    console.log(temp["PagerVM"]["Records"][0]["LicenseStatusTypeName"])
-    console.log(temp["PagerVM"]["Records"][0]["ExpirationDate"])
+    var user_id = temp["PagerVM"]["Records"][0]["IndividualId"];
+    var options = {
+      'method': 'GET',
+      'url': 'https://ws.nvdental.org/api/Individual/VerifyLicenseSearchBYIndividualId?IndividualId='+user_id,
+      'headers': {
+        'Content-Type': 'application/json'
+      },
+    };
+
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+        //console.log(response.body);
+        var temp1 = JSON.parse(response.body);
+        console.log(temp1["lstVerifyLicenseSearchResponse"][0]["FirstName"],temp1["lstVerifyLicenseSearchResponse"][0]["LastName"]);
+        console.log(temp1["lstVerifyLicenseSearchResponse"][0]["LicenseStatusTypeName"]);
+        console.log(temp1["lstVerifyLicenseSearchResponse"][0]["ExpirationDate"]);
+        if (temp1["lstVerifyLicenseSearchResponse"][0]["MalpracticeList"][0]["ActionType"] != null)
+        {
+          console.log("Yes")
+        }
+        else
+        {
+          console.log("No")
+        }
+
+      
+    });
 });
 
 
--- routes/nevada.js
+++ routes/nevada.js
@@ -24,23 +24,48 @@ router.get('/:lic_id',  function(req, res){
     };
     request(options, function (error, response) {
         if (error) throw new Error(error);
-        console.log(response.body);
+        //console.log(response.body);
         var temp  = JSON.parse(response.body);
         user_id = temp["PagerVM"]["Records"][0]["IndividualId"];
-        console.log(temp["PagerVM"]["Records"][0]["FirstName"],temp["PagerVM"]["Records"][0]["MiddleName"],temp["PagerVM"]["Records"][0]["LastName"])
-        console.log(temp["PagerVM"]["Records"][0]["LicenseStatusTypeName"])
-        console.log(temp["PagerVM"]["Records"][0]["ExpirationDate"])
-        //Div container - Dheeraj mistake. - Disciplinary
-        var name = temp["PagerVM"]["Records"][0]["FirstName"]+ " " + temp["PagerVM"]["Records"][0]["MiddleName"] + " " + temp["PagerVM"]["Records"][0]["LastName"];
-        var result = [name,temp["PagerVM"]["Records"][0]["LicenseStatusTypeName"],temp["PagerVM"]["Records"][0]["ExpirationDate"],"No"]
-        if(is_api == "true"){
-            res.send(JSON.stringify(result));
-        }
-        else{
-            res.render("pages/status", {result: JSON.stringify(result)})
-        }
+        
+        var options = {
+            'method': 'GET',
+            'url': 'https://ws.nvdental.org/api/Individual/VerifyLicenseSearchBYIndividualId?IndividualId='+user_id,
+            'headers': {
+              'Content-Type': 'application/json'
+            },
+          };
+      
+          request(options, function (error, response) {
+            if (error) throw new Error(error);
+              //console.log(response.body);
+              var temp1 = JSON.parse(response.body);
+              var disci = ""
+              //console.log(temp1["lstVerifyLicenseSearchResponse"][0]["FirstName"],temp1["lstVerifyLicenseSearchResponse"][0]["LastName"]);
+              //console.log(temp1["lstVerifyLicenseSearchResponse"][0]["LicenseStatusTypeName"]);
+              //console.log(temp1["lstVerifyLicenseSearchResponse"][0]["ExpirationDate"]);
+              //console.log(temp1["lstVerifyLicenseSearchResponse"][0]["MalpracticeList"].length);
+              
+              if (temp1["lstVerifyLicenseSearchResponse"][0]["MalpracticeList"].length != 0)
+              {
+                //temp1["lstVerifyLicenseSearchResponse"][0]["MalpracticeList"][0]["ActionType"] != null
+                disci = "Yes"
 
-
+              }
+              else
+              {
+                disci = "No"
+              }
+      
+              var name = temp1["lstVerifyLicenseSearchResponse"][0]["FirstName"] + temp1["lstVerifyLicenseSearchResponse"][0]["LastName"]
+              var result = [name,temp1["lstVerifyLicenseSearchResponse"][0]["LicenseStatusTypeName"],temp1["lstVerifyLicenseSearchResponse"][0]["ExpirationDate"],disci]
+              if(is_api == "true"){
+                  res.send(JSON.stringify(result));
+              }
+              else{
+                  res.render("pages/status", {result: JSON.stringify(result)})
+              }
+          });
     });
 });
 
