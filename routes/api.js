98453819b3112986790eae9ead0a871f7e5720a7
dheeraj-komandur <dheerajkomandur@gmail.com>
2020-07-06 17:40:02 +0530

    url query json  and set deploy url

--- routes/api.js
+++ routes/api.js
@@ -18,10 +18,13 @@ router.use(function (req,res,next){
 })
 
 router.get('/', function (req,res) {
-    var arr = JSON.parse(req.query.array);
-    console.log(arr);
-    var state = arr[0];
-    var license_num = arr[1];
+    //var arr = JSON.parse(req.query.array);
+    //console.log(arr);
+    //var state = arr[0];
+    //var license_num = arr[1];
+    var state = req.query.state;
+    var license_num = req.query.lic_num;
+    console.log(state)
     if(state == undefined || license_num == undefined || state == "" || license_num == ""){
         res.send(404);
         console.log("Input error");
@@ -31,7 +34,8 @@ router.get('/', function (req,res) {
     else {
         var is_api = true;
         var result = [state, license_num];
-        var license_url = "http://localhost:5000/api/license/" + state + "/" + license_num + "?is_api=" + is_api;
+        //var license_url = "http://localhost:5000/api/license/" + state + "/" + license_num + "?is_api=" + is_api;
+        var license_url = "http://pg-hackathon.herokuapp.com/api/license/" + state + "/" + license_num + "?is_api=" + is_api;
         //var npi_url = "http://localhost:5000/api/npi/" + npi + "?is_api="+is_api;
         request.get(license_url, function (error, response, body) {
             if(error){
